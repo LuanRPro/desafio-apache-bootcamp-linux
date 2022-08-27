06d47cdc2bcc4a465426c95ec1026768d88b984f
Luan Reis <96321289+LuanRPro@users.noreply.github.com>
2022-08-27 00:49:50 -0300

    commit do projeto

---
+++ scriptApacheDio.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Verificando e atualizando o servidor..."
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+
+echo "Baixando arquivos da fonte e copiando para a aplicação..."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+
